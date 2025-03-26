934d7dfd66feca6768ae49f4fbcde486e349f219
caue <cauegon12@gmail.com>
2025-03-26 20:42:49 -0300

    teste

---
+++ Controllers/PersonagemController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Xablau.Data;
+
+namespace Xablau.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonagemController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public PersonagemController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+    }
+}
