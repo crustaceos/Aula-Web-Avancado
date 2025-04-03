b555331431638ed2ee7007430f13194a01160bdd
caue <cauegon12@gmail.com>
2025-04-02 21:18:18 -0300

    Metodos PUT GET e POST

--- Controllers/PersonagemController.cs
+++ Controllers/PersonagemController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
 using Xablau.Data;
 using Xablau.Models;
 
@@ -38,6 +39,42 @@ namespace Xablau.Controllers
             var personagens = await _appDbContext.XablauDB.ToListAsync();
 
             return StatusCode(201,personagens);
+    
+        }
+
+        [HttpGet("{id}")]
+
+        ///ENUMERABLE PARA FZER GET 
+        public async Task<ActionResult<IEnumerable<Personagem>>> GetPersonagemId(int id)
+        {
+            var buscarPersonagem = await _appDbContext.XablauDB.FindAsync(id);
+            
+            if(buscarPersonagem == null){
+                return NotFound("Personagem não encontrado");
+            }
+
+            return StatusCode(201,buscarPersonagem);
+    
+        }
+
+        ///PARA FAZER PUT
+
+        [HttpPut("{id}")]
+
+        public async Task<IActionResult> UpdatePersonagemId(int id, [FromBody] Personagem personagemAtualizado)
+        {
+            var buscarPersonagem = await _appDbContext.XablauDB.FindAsync(id);
+            
+            if(buscarPersonagem == null){
+                return NotFound("Personagem não encontrado");
+            }
+
+            ///ENTRY SERVE PARA DIZER QUAL OBJETO EU VOU QUERER ALTERAR DA TABELA, DEPOIS DIGO O VALOR QUE ELE VAI PASSAR A TER
+            _appDbContext.Entry(buscarPersonagem).CurrentValues.SetValues(personagemAtualizado);
+
+            await _appDbContext.SaveChangesAsync();
+
+            return Ok("Atualizado com sucesso");
     
         }
 
